1d82fc09abb3c22c744a884fede7f00a1439568a
Maimell <malinka77@gmail.com>
2019-11-24 16:46:58 -0700

    sequelize sync forse false

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 require("./controllers/burgers_controller.js")(app);
 // =============================================================
 
-db.sequelize.sync({ force: true }).then(function () {
+db.sequelize.sync({ force: false }).then(function () {
     app.listen(PORT, function () {
         console.log("App listening on PORT " + PORT);
     });
